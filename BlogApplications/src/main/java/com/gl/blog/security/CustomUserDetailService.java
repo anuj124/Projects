6de6aff49cc42ebb70fdf50c12995b88ab909115
Anuj Sharma <53485174+anuj124@users.noreply.github.com>
2024-01-03 12:38:59 +0530

    Add files via upload

--- BlogApplications/src/main/java/com/gl/blog/security/CustomUserDetailService.java
+++ BlogApplications/src/main/java/com/gl/blog/security/CustomUserDetailService.java
@@ -9,10 +9,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.gl.blog.entity.User;
 import com.gl.blog.repository.UserRepository;
 
+@Service
 public class CustomUserDetailService implements UserDetailsService {
 
 	@Autowired
